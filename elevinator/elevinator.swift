5a818dafe17faeed2b17cd83ca0eab7ee80bd56d
Mark Eschbach <meschbach@gmail.com>
2025-11-04 20:45:11 -0800

    Initial Commit

---
+++ elevinator/elevinator.swift
@@ -0,0 +1,9 @@
+//
+//  elevinator.swift
+//  elevinator
+//
+//  Created by Mark Eschbach on 11/4/25.
+//
+
+import Foundation
+
---
+++ elevinatorTests/elevinatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  elevinatorTests.swift
+//  elevinatorTests
+//
+//  Created by Mark Eschbach on 11/4/25.
+//
+
+import Testing
+@testable import elevinator
+
+struct elevinatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
